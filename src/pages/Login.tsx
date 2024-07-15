c7a2d13f99be6aa833c35974abbe475c5bf76524
mkawsar <kawsar.diu888@gmail.com>
2024-07-15 14:41:01 +0600

    validation added for login form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,14 +1,30 @@
-import React, {useEffect} from 'react';
+import * as Yup from 'yup';
+import React, { useEffect, useState } from 'react';
 
 const Login: React.FC = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState({});
+
     useEffect(() => {
         document.title = 'Admin | Login';
         document.body.classList.add('bg-gradient-primary')
         return function cleanup() {
             document.body.classList.remove('bg-gradient-primary');
         };
-        // document.head.title
     }, []);
+
+    const validation = Yup.object().shape({
+        email: Yup.string().email().required(),
+    });
+
+    const handleSubmitLoginForm = async (e: React.FormEvent) => {
+        e.preventDefault();
+        const form = {
+            email: email
+        };
+        const formErrors = validation.validate(form);
+    }
 
     return (
         <div className="container">
@@ -23,12 +39,12 @@ const Login: React.FC = () => {
                                             <div className="text-center">
                                                 <h1 className="h4 text-gray-900 mb-4">Welcome Back!</h1>
                                             </div>
-                                            <form className="user">
+                                            <form className="user" onSubmit={handleSubmitLoginForm}>
                                                 <div className="form-group">
-                                                    <input type="email" className="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address..." />
+                                                    <input type="email" className="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address..." value={email} onChange={(e) => setEmail(e.target.value)} />
                                                 </div>
                                                 <div className="form-group">
-                                                    <input type="password" className="form-control form-control-user" id="exampleInputPassword" placeholder="Password" />
+                                                    <input type="password" className="form-control form-control-user" id="exampleInputPassword" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
                                                 </div>
                                                 <div className="form-group">
                                                     <div className="custom-control custom-checkbox small">
@@ -37,7 +53,7 @@ const Login: React.FC = () => {
                                                         Me</label>
                                                     </div>
                                                 </div>
-                                                <a href="index.html" className="btn btn-primary btn-user btn-block">Login</a>
+                                                <button type='submit' className="btn btn-primary btn-user btn-block">Login</button>
                                                 <hr />
                                                 <a href="index.html" className="btn btn-google btn-user btn-block">
                                                     <i className="fab fa-google fa-fw" /> Login with Google
